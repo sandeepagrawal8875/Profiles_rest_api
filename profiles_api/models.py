f53be64fd7d9e6969881657f15f9f16f39635032
sunny <sandeepagrawal8875@gmail.com>
2020-07-19 21:39:19 +0530

    authenticated with end user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from django.conf import settings
 
 class UserProfileManager(BaseUserManager):
     """ manager for user profiles"""
@@ -51,3 +51,14 @@ class UserProfile( AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
+
+class ProfileFeedItem(models.Model):
+    """profile status update"""
+    
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
+
